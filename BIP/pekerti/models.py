4f98e4a18ea234a993708dc0d4666f54f5068c85
herlangga72 <herlangga72@gmail.com>
2021-09-29 00:17:19 +0700

    add design on data structure

--- BIP/pekerti/models.py
+++ BIP/pekerti/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+class (models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
