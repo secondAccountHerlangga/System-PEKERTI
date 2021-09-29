3fcc505f24ba34524257ab9831f0c035a003aa92
herlangga72 <herlangga72@gmail.com>
2021-09-30 04:40:20 +0700

    add admin models

--- BIP/pekerti/models.py
+++ BIP/pekerti/models.py
@@ -1,6 +1,11 @@
 from djongo import models
 
-# Create your models here.
-class (models.Model):
-    name = models.CharField(max_length=100)
-    tagline = models.TextField()
+# reference https://www.djongomapper.com/using-django-with-mongodb-data-fields/#nesting-embedded-fields
+class Permission(models.Model):
+    validate_data = models.BooleanField()
+    update_data_peserta = models.BooleanField()
+    add_other_user =models.BooleanField()
+class Admin(models.Model):
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+    permission = models.EmbeddedField(model_container=Permission)
