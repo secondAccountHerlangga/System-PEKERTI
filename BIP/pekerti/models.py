d22c4989d2179194a2ec2406162d5765efcde524
herlangga72 <herlangga72@gmail.com>
2021-09-30 04:51:51 +0700

    Peserta Models

--- BIP/pekerti/models.py
+++ BIP/pekerti/models.py
@@ -5,7 +5,24 @@ class Permission(models.Model):
     validate_data = models.BooleanField()
     update_data_peserta = models.BooleanField()
     add_other_user =models.BooleanField()
+
 class Admin(models.Model):
     username = models.CharField(max_length=32)
     password = models.CharField(max_length=32)
     permission = models.EmbeddedField(model_container=Permission)
+
+class Peserta_data(models.Model):
+    linkOpenLearning = models.CharField(max_length=50)
+    linkAksesKelas = models.CharField(max_length=50)
+    linkSertifikat = models.CharField(max_length=50)
+    
+class Peserta(models.Model):
+    nama = models.CharField(max_length=50)
+    nidn = NotImplemented
+    institusi_asal = NotImplemented
+    email = models.EmailField(max_length=254)
+    nomer_telp  = models.PhoneNumberField()
+    link_transfer = models.CharField( max_length=50)
+    link_penugasan = models.CharField(max_length=50)
+    link_tidak_menggugat = models.CharField(max_length=50)
+    permission = models.EmbeddedField(model_container=Peserta_data)
